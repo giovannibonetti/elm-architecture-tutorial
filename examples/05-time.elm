f5bc84941587de38ea2148948bd66a5979003264
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2018-08-23 20:54:20 -0300

    clock with pause button

--- examples/05-time.elm
+++ examples/05-time.elm
@@ -1,5 +1,6 @@
 import Browser
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Task
 import Time
 
@@ -24,12 +25,13 @@ main =
 type alias Model =
   { zone : Time.Zone
   , time : Time.Posix
+  , paused : Bool
   }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model Time.utc (Time.millisToPosix 0)
+  ( Model Time.utc (Time.millisToPosix 0) False
   , Task.perform AdjustTimeZone Time.here
   )
 
@@ -41,7 +43,7 @@ init _ =
 type Msg
   = Tick Time.Posix
   | AdjustTimeZone Time.Zone
-
+  | Pause
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -57,6 +59,11 @@ update msg model =
       , Cmd.none
       )
 
+    Pause ->
+      ( { model | paused = (not model.paused) }
+      , Cmd.none
+      )
+
 
 
 -- SUBSCRIPTIONS
@@ -64,7 +71,10 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every 1000 Tick
+  if model.paused then
+    Time.every 1000000000 Tick
+  else
+    Time.every 1000 Tick
 
 
 
@@ -74,8 +84,12 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    hour   = String.fromInt (Time.toHour   model.zone model.time)
-    minute = String.fromInt (Time.toMinute model.zone model.time)
-    second = String.fromInt (Time.toSecond model.zone model.time)
+    intToPaddedStr int = String.padLeft 2 '0' (String.fromInt int)
+    hour   = intToPaddedStr (Time.toHour   model.zone model.time)
+    minute = intToPaddedStr (Time.toMinute model.zone model.time)
+    second = intToPaddedStr (Time.toSecond model.zone model.time)
   in
-  h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+    div []
+      [ h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+      ,  button [ onClick Pause ] [ text "Pause" ]
+      ]
